726a5f0edfbe3d49ba2d75b628fe99b9d6cff20b
khris <khris56@hotmail.com>
2018-10-04 12:14:22 -0500

    intervencion del foxy

--- js/charts.js
+++ js/charts.js
@@ -1,7 +1,7 @@
 var myCircle = Circles.create({
   id:                  'circles-1',
   radius:              60,
-  value:               80,
+  value:               100,
   maxValue:            100,
   width:               5,
   text:                function(value){return value + '%';},
@@ -17,7 +17,7 @@ var myCircle = Circles.create({
 var myCircle = Circles.create({
   id:                  'circles-2',
   radius:              60,
-  value:               80,
+  value:               100,
   maxValue:            100,
   width:               5,
   text:                function(value){return value + '%';},
@@ -65,7 +65,7 @@ var myCircle = Circles.create({
 var myCircle = Circles.create({
   id:                  'circles-5',
   radius:              60,
-  value:               60,
+  value:               90,
   maxValue:            100,
   width:               5,
   text:                function(value){return value + '%';},
@@ -97,7 +97,7 @@ var myCircle = Circles.create({
 var myCircle = Circles.create({
   id:                  'circles-7',
   radius:              60,
-  value:               60,
+  value:               70,
   maxValue:            100,
   width:               5,
   text:                function(value){return value + '%';},
@@ -113,7 +113,7 @@ var myCircle = Circles.create({
 var myCircle = Circles.create({
   id:                  'circles-8',
   radius:              60,
-  value:               60,
+  value:               90,
   maxValue:            100,
   width:               5,
   text:                function(value){return value + '%';},
@@ -145,7 +145,7 @@ var myCircle = Circles.create({
 var myCircle = Circles.create({
   id:                  'circles-10',
   radius:              60,
-  value:               80,
+  value:               100,
   maxValue:            100,
   width:               5,
   text:                function(value){return value + '%';},
@@ -161,7 +161,7 @@ var myCircle = Circles.create({
 var myCircle = Circles.create({
   id:                  'circles-11',
   radius:              60,
-  value:               80,
+  value:               100,
   maxValue:            100,
   width:               5,
   text:                function(value){return value + '%';},
@@ -177,7 +177,23 @@ var myCircle = Circles.create({
 var myCircle = Circles.create({
   id:                  'circles-12',
   radius:              60,
-  value:               80,
+  value:               100,
+  maxValue:            100,
+  width:               5,
+  text:                function(value){return value + '%';},
+  colors:              ['#FFF', '#00bcd4'],
+  duration:            400,
+  wrpClass:            'circles-wrp',
+  textClass:           'circles-text',
+  valueStrokeClass:    'circles-valueStroke',
+  maxValueStrokeClass: 'circles-maxValueStroke',
+  styleWrapper:        true,
+  styleText:           true
+});
+var myCircle = Circles.create({
+  id:                  'circles-13',
+  radius:              60,
+  value:               60,
   maxValue:            100,
   width:               5,
   text:                function(value){return value + '%';},
